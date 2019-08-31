7eda9d9e5bfccdb2230abe44fbed61a101aa0261
Nopelands <rhas@cin.ufpe.br>
2019-08-31 10:42:52 -0300

    Create legal_move_generator.py
    haha these should all have been separate commits

---
+++ legal_move_generator.py
@@ -0,0 +1,43 @@
+def main():
+
+
+def generator(fen_board, x, y):
+
+
+def pseudo_legal_generator(board, x, y):
+
+
+def square_is_under_attack(x, y):
+
+
+def piece_moves(piece):
+
+
+def is_square_inside_board(x, y):
+    answer = 0 <= x < 8 and 0 <= y < 8
+    return answer
+
+def fen_to_board(fen):
+    answer = []
+
+    return answer
+
+class Board:
+    board = []
+    player_to_move = ""
+    castling_rights = []
+    en_passant_target = []
+
+    def __init__(self, fen):
+        info = fen_to_board(fen)
+        board = info[0]
+        player_to_move = info[1]
+        castling_rights = info[2]
+        en_passant_target = info[3]
+        
+    def is_piece_in_square(self, x, y): #remove LBYL?
+
+    def get_piece_in_square(self, x, y):
+
+if __name__ == '__main__':
+    main()

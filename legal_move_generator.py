c655c83d336bcf38d49ffa71080ea73207889f90
Nopelands <rhas@cin.ufpe.br>
2019-09-02 01:09:50 -0300

    fen to board function done

--- legal_move_generator.py
+++ legal_move_generator.py
@@ -1,26 +1,44 @@
 def main():
+    print(fen_to_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -"))
 
 
-def generator(fen_board, x, y):
+# def generator(fen_board, x, y):
+#
+#
+# def pseudo_legal_generator(board, x, y):
+#
+#
+# def square_is_under_attack(x, y):
+#
+#
+# def piece_moves(piece):
 
 
-def pseudo_legal_generator(board, x, y):
-
-
-def square_is_under_attack(x, y):
-
-
-def piece_moves(piece):
-
-
-def is_square_inside_board(x, y): #remove LBYL?
+def is_square_inside_board(x, y):  # remove LBYL?
     answer = 0 <= x < 8 and 0 <= y < 8
     return answer
 
+
 def fen_to_board(fen):
     answer = []
-
+    info_list = fen.split(" ")
+    board_state = info_list[0].split("/")
+    final_board = [[] for i in range(8)]
+    for i in range(len(board_state)):
+        for j in range(len(board_state[i])):
+            if board_state[i][j].isnumeric():
+                for k in range(int(board_state[i][j])):
+                    final_board[i].append("empty")
+            else:
+                final_board[i].append(board_state[i][j])
+    answer.append(final_board)
+    answer.append(info_list[1])
+    answer.append(info_list[2])
+    if len(info_list[3]) > 1:  # converts notation to list index using magic numbers
+        info_list[3] = ord(info_list[3][0]) - 96 + info_list[3][1]
+    answer.append(info_list[3])
     return answer
+
 
 class Board:
     board = []
@@ -35,9 +53,10 @@ class Board:
         castling_rights = info[2]
         en_passant_target = info[3]
 
-    def is_piece_in_square(self, x, y): #remove LBYL?
+    # def is_piece_in_square(self, x, y): #remove LBYL?
+    #
+    # def get_piece_in_square(self, x, y):
 
-    def get_piece_in_square(self, x, y):
 
 if __name__ == '__main__':
     main()
